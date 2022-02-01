681a77bc7d06c190aa8435e23e5ffc801e760bca
Jay Kim <48934750+jsk95@users.noreply.github.com>
2022-02-01 16:52:27 +1300

    Type to event type (#3)
    * Updating Invoicr.EventFeed mock
    
    * Update InvoiceEvent.cs
    
    Co-authored-by: Jay Kim <jay.kim@xero.com>

--- InvoicrApp/Models/InvoiceEvent.cs
+++ InvoicrApp/Models/InvoiceEvent.cs
@@ -5,7 +5,7 @@ namespace InvoicrApp.Models
     public class InvoiceEvent
     {
         public long Id { get; set; }
-        public string Type { get; set; }
+        public string EventType { get; set; }
         public Invoice Content { get; set; }
         public DateTime CreatedDateUtc { get; set; }
     }
